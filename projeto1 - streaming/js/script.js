67d9683dd00254e7fb03591227141277e714d25e
Phelipe Alves <ultimaterafa79@gmail.com>
2024-08-13 20:36:22 -0300

    Add files via upload
    Commit de atualização (13 de agosto de 2024)

---
+++ projeto1 - streaming/js/script.js
@@ -0,0 +1,12 @@
+// Pág. Index
+let btn_logo = document.querySelector(".logo-name")
+
+btn_logo.addEventListener("click", (evt) => {
+    window.location = "../html/index.html";
+})
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// Pág. Register
